c362f5299f036326af7ee08f7809ef87481ca97b
sherka <154922248+sherka-57@users.noreply.github.com>
2026-01-07 17:40:57 -0500

    Update supabaseClient.js

--- public/src/supabaseClient.js
+++ public/src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://sazdtrcayjljgmihilkj.supabase.co';
-const supabaseKey = 'sb_publishable_8PjevXnceWHX2i_xoB-M7g_UYzHxq5c'; // public anon key
+const supabaseKey = 'sb_publishable_8PjevXnceWHX2i_xoB-M7g_UYzHxq5c'; // NOT service role key
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
